0a7373564190eed911b9ecde13ba6e36dd9cd07b
Ingo Düppe <ingo.dueppe@crowdcode.io>
2016-07-21 11:07:51 +0200

    Fixed Method in Auction

--- auction-core/src/main/java/io/crowdcode/speedbay/auction/model/Auction.java
+++ auction-core/src/main/java/io/crowdcode/speedbay/auction/model/Auction.java
@@ -39,7 +39,8 @@ public class Auction extends AbstractEntity<Auction, Long> {
     }
 
     public boolean isRunning() {
-        return beginDate.isBefore(TimeMachine.now()) && expireDate.isAfter(TimeMachine.now());
+        return beginDate.compareTo(TimeMachine.now()) <= 0
+                && expireDate.isAfter(TimeMachine.now());
     }
 
     public String getOwner() {
